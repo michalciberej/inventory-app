974327181e6124972529ef029d411c789178ff11
Michal Ciberej <michal.ciberej@gmail.com>
2024-04-20 16:29:23 +0200

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+import express from 'express';
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('its working');
+});
+
+app.listen(3000, () => console.log('Server is listening on port 3000!'));
