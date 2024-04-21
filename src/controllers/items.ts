3b6f7c795e308ee5293f5f9bc9387595bf998c58
Michal Ciberej <michal.ciberej@gmail.com>
2024-04-21 15:25:29 +0200

    Add item views, controller, styles

--- src/controllers/items.ts
+++ src/controllers/items.ts
@@ -1,12 +1,36 @@
 import express from 'express';
 import asyncHandler from 'express-async-handler';
+import { Item } from '../models/item';
 
 const router = express.Router();
 
 router.get(
   '/',
-  asyncHandler((req, res, next) => {
-    res.render('items');
+  asyncHandler(async (req, res) => {
+    const items = await Item.find({});
+    console.log(items);
+    res.render('items', { items });
+  })
+);
+
+router.get(
+  '/:id',
+  asyncHandler(async (req, res) => {
+    try {
+      const item = await Item.findById({ _id: req.params.id });
+      res.render('itemById', { item });
+    } catch (err) {
+      console.log(err);
+      res.redirect('/items'); //! CHANGE FOR NOTFOUND PAGE
+    }
+  })
+);
+
+router.post(
+  '/:id',
+  asyncHandler(async (req, res) => {
+    await Item.deleteOne({ _id: req.params.id });
+    res.redirect('/items');
   })
 );
 
